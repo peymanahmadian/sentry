7fc9535b02651b7754c7e4a7cde9e638d542650d
Peyman Ahmadian <peyman@Peymans-MacBook-Pro.local>
2023-07-18 17:20:47 +0330

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import './App.css'
 import { ErrorBoundary } from '@sentry/react';
 function App() {
   const [count, setCount] = useState(0)
-
+  const [arr,setArr]=useState([1,2])
   return (
     <ErrorBoundary
-      fallback={<div>error</div>}
+      fallback={<div>error ocuured</div>}
     >
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -20,9 +20,15 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count / a)}>
+        <button onClick={() => setCount((count) => (count / Infinity) - 3)}>
           count is {count}
         </button>
+        <button onClick={()=>setArr("sample")}>
+          convert to string
+        </button>
+        {
+          arr.map(i=><div key={i}>arr is {i}</div>)
+        }
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
