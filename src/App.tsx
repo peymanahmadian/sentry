3e263a6867127d4100cfa77885c395afef754786
Peyman Ahmadian <peyman@Peymans-MacBook-Pro.local>
2023-07-18 17:02:59 +0330

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { ErrorBoundary } from '@sentry/react';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ErrorBoundary
+      fallback={<div>error</div>}
+    >
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,7 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count / a)}>
           count is {count}
         </button>
         <p>
@@ -28,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </ErrorBoundary>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import * as Sentry from "@sentry/react";
+Sentry.init({
+  dsn: "https://8a17de027d4d49c0a8eda1e24ca81fc4@o1248525.ingest.sentry.io/4505544731459584",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["https://sentry-ruby.vercel.app/"],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
